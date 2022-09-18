2f5b2b4c382eeeb3abcbd1da4565356f2d127093
AlanDeStraya <alandestraya@gmail.com>
2022-09-18 15:54:54 -0400

    still not working

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const server = require('http').createServer(app);
 const port = process.env.PORT || 31415;
 
 const path = require('path');
-const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+app.use(express.static(path.join(__dirname + '/public')));
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
