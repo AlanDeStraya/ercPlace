f8486ef5aa53681f8fcb0b18f1f9cfa8a0be6be0
Alan deStraya <alandestraya@gmail.com>
2022-10-08 23:33:18 -0400

    fixing server code bug for socket io test

--- server/server.js
+++ server/server.js
@@ -129,34 +129,34 @@ socket.use(([event, ...args], next) => {
 
 
 // basic emit
-  socket.emit(/* ... */);
+  socket.emit(...);
 
   // to all clients in the current namespace except the sender
-  socket.broadcast.emit(/* ... */);
+  socket.broadcast.emit(...);
 
   // to all clients in room1 except the sender
-  socket.to("room1").emit(/* ... */);
+  socket.to("room1").emit(...);
 
   // to all clients in room1 and/or room2 except the sender
-  socket.to("room1").to("room2").emit(/* ... */);
+  socket.to("room1").to("room2").emit(...);
 
   // to all clients in room1
-  io.in("room1").emit(/* ... */);
+  io.in("room1").emit(...);
 
   // to all clients in namespace "myNamespace"
-  io.of("myNamespace").emit(/* ... */);
+  io.of("myNamespace").emit(...);
 
   // to all clients in room1 in namespace "myNamespace"
-  io.of("myNamespace").to("room1").emit(/* ... */);
+  io.of("myNamespace").to("room1").emit(...);
 
   // to individual socketid (private message)
-  io.to(socketId).emit(/* ... */);
+  io.to(socketId).emit(...);
 
   // to all clients on this node (when using multiple nodes)
-  io.local.emit(/* ... */);
+  io.local.emit(...);
 
   // to all connected clients
-  io.emit(/* ... */);
+  io.emit(...);
 
   // WARNING: `socket.to(socket.id).emit()` will NOT work, as it will send to everyone in the room
   // named `socket.id` but the sender. Please use the classic `socket.emit()` instead.
