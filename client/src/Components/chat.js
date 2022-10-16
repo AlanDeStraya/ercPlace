283a4399f3ed7eb2fc1cc39bd031b610a49904d8
root <alandestraya@gmail.com>
2022-10-16 00:28:34 +0000

    build for chat functions

--- client/src/Components/chat.js
+++ client/src/Components/chat.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Chat = ({ socket }) => {
 	
 	const [msgs, setMsgs] = useState([]);
 	const [typing, setTyping] = useState('');
+	const [sendMsg, setSendMsg] = useState('');
 	
 	const chatWindow = document.getElementById('chat-window');
 	
 	useEffect(() => {
-		
-	},[msgs]}
-		
+		console.log('chg msg');
+	},[msgs]);
+	
 	
 //get username, from commslive before opening chat
+	const username = 'User1';
 	socket.emit('cNewUser', username);
 	let timer;
 	let users;
 	
-	socket.on('sUserConnected, name => {
+	socket.on('sUserConnected', name => {
 		renderMessage(`${name} joined`);
 	});
 	
-	socket.on('sNewUserPackage', package => {
-		console.log(package.sUsers);
-		users = package.sUsers;
+	socket.on('sNewUserPackage', pack => {
+		console.log(pack.sUsers);
+		users = pack.sUsers;
 		setMsgs(() => {
-			return [...messages];
+			return [...msgs];
 		});
 	});
 
 	socket.on('sChatMessage', obj => {
-		renderMessate(obj);
+		renderMessage(obj);
 	});
 
 	socket.on('sUserDisconnected', obj => {
@@ -38,7 +41,7 @@ const Chat = ({ socket }) => {
 	});
 
 	socket.on('sTyping', name => {
-		setTyping(() => name});
+		setTyping(() => name);
 		chatWindow.scrollTop = chatWindow.scrollHeight;
 	});
 	
@@ -50,7 +53,7 @@ const Chat = ({ socket }) => {
 	function stopTyping() {
 		setTyping(() => '');
 		socket.emit('notTyping', username);
-	});
+	};
 	
 	function handleTyping(event) {
 		if(event.code === 'Enter') {
@@ -73,13 +76,13 @@ const Chat = ({ socket }) => {
 		if(typing) {
 			stopTyping();
 		}
-		socket.emit('cChatMessage', message);
-		chatInput.value = '';
+		socket.emit('cChatMessage', sendMsg);
+		setSendMsg(() => '');
 	};
 	
 	function renderMessage(message) {
 		setMsgs(() => {
-			return [...messages, obj];
+			return [...msgs, message];
 		});
 		chatWindow.scrollTop = chatWindow.scrollHeight;
 	};
@@ -91,12 +94,21 @@ const Chat = ({ socket }) => {
 	return (
 		<div id='chat'>
 			<div id='chat-window'>
-				<div id='chat-window-messages'></div>
+				<div id='chat-window-messages'>
+					{[msgs]
+					.sort((a, b) => a.timestamp - b.timestamp)
+					.map((message) => (
+						<div key={message.messageId}>
+							<em>{`${message.timestamp}, ${message.sender}`}</em>
+							<p>{`${message.message}`}</p>
+						</div>
+					))}
+				</div>
 				{typing && <div id='chat-window-typing'>{`${typing} is typing...`}</div>}
 			</div>
-			<form id='chat-form' autocomplete='off'>
+			<form id='chat-form' autocomplete='off' onSubmit={submitChat}>
 				<label id='chat-label' for='chat-input'>&nbsp;Chat:&nbsp;</label>
-				<input className='input' id='chat-input' onKeyPress={handleTyping} />
+				<input className='input' id='chat-input' onKeyPress={handleTyping} onChange={e => setSendMsg(e.currentTarget.value)} />
 				<input id='submit' type='submit' value=' send ' />
 			</form>
 		</div>
