7ce4c2b2e4dcd93b84b84d36ab23f0e0a51f7ec3
root <alandestraya@gmail.com>
2022-10-17 20:29:41 +0000

    Changing alert css, attempting areaSelector box bug fix

--- client/src/Components/areaSelector.js
+++ client/src/Components/areaSelector.js
@@ -39,6 +39,7 @@ const AreaSelector = ({ diversionState, setDiversionState, socket }) => {
   */
 
   function createArea(areaName) {
+	console.log('creating area');
     return (
       <div className='area-container' key={areaName}>
         <input onCheckboxChange={handleAreaChange} isSelected={areas[areaName]} type='checkbox' className='area-checkbox' id={`${areaName}-box`} />
@@ -48,6 +49,7 @@ const AreaSelector = ({ diversionState, setDiversionState, socket }) => {
   };
 
   function createAreasOne() {
+	console.log('creating areasss');
     areaArray.slice(0, areaArray.legnth / 2).map(a => createArea(a));
   };
   function createAreasTwo() {
