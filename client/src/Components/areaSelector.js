3499d15fc33c21422ff8bc2c1ed9ed1959c9bf18
root <alandestraya@gmail.com>
2022-10-17 22:59:15 +0000

    Still trying to fix it

--- client/src/Components/areaSelector.js
+++ client/src/Components/areaSelector.js
@@ -57,6 +57,7 @@ const AreaSelector = ({ diversionState, setDiversionState, socket }) => {
     return areaArray.slice(0, areaArray.length / 2).map(a => createArea(a));
   };
   function createAreasTwo() {
+	console.log('creating ares 222');
     areaArray.slice(areaArray.length / 2).map(a => createArea(a));
   };
 
@@ -65,102 +66,13 @@ const AreaSelector = ({ diversionState, setDiversionState, socket }) => {
   ///////////////////////////////////////////////////////////////////////////////////
   good explanation for checkboxes
   http://react.tips/checkboxes-in-react-16/
-  
-   import React, { Component } from "react";
-import Checkbox from "./Checkbox";
-const OPTIONS = ["One", "Two", "Three"];
-class App extends Component {
-  state = {
-    checkboxes: OPTIONS.reduce(
-      (options, option) => ({
-        ...options,
-        [option]: false
-      }),
-      {}
-    )
-  };
-  selectAllCheckboxes = isSelected => {
-    Object.keys(this.state.checkboxes).forEach(checkbox => {
-      this.setState(prevState => ({
-        checkboxes: {
-          ...prevState.checkboxes,
-          [checkbox]: isSelected
-        }
-      }));
-    });
-  };
-  selectAll = () => this.selectAllCheckboxes(true);
-  deselectAll = () => this.selectAllCheckboxes(false);
-  handleCheckboxChange = changeEvent => {
-    const { name } = changeEvent.target;
-    this.setState(prevState => ({
-      checkboxes: {
-        ...prevState.checkboxes,
-        [name]: !prevState.checkboxes[name]
-      }
-    }));
-  };
-  handleFormSubmit = formSubmitEvent => {
-    formSubmitEvent.preventDefault();
-
-    Object.keys(this.state.checkboxes)
-      .filter(checkbox => this.state.checkboxes[checkbox])
-      .forEach(checkbox => {
-        console.log(checkbox, "is selected.");
-      });
-  };
-  createCheckbox = option => (
-    <Checkbox
-      label={option}
-      isSelected={this.state.checkboxes[option]}
-      onCheckboxChange={this.handleCheckboxChange}
-      key={option}
-    />
-  );
-
-  createCheckboxes = () => OPTIONS.map(this.createCheckbox);
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col-sm-12">
-            <form onSubmit={this.handleFormSubmit}>
-              {this.createCheckboxes()}
-
-              <div className="form-group mt-2">
-                <button
-                  type="button"
-                  className="btn btn-outline-primary mr-2"
-                  onClick={this.selectAll}
-                >
-                  Select All
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-outline-primary mr-2"
-                  onClick={this.deselectAll}
-                >
-                  Deselect All
-                </button>
-                <button type="submit" className="btn btn-primary">
-                  Save
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-  */
+*/
 
     return (
 		<div id='area-selector'>
 			<p id='area-instruction'>Click all obstructed areas (click again to de-select)</p>
 			<div id='track-one-areas'>
-				{createArea('area-one-one')}
+				{createAreasOne()}
 			</div>
 			<div className='labels'>
 				<p>TUN-W</p>&nbsp;
